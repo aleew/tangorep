48147c33080db5deb7a6179761e380f518e5d6c2
Samu Lehtonen <samu.lehtonen@hotmail.com>
2013-10-07 18:50:32 +0300

    valikko

--- git.test/src/git/test/GitTest.java
+++ git.test/src/git/test/GitTest.java
@@ -1,10 +1,31 @@
 package git.test;
 
+import java.util.Scanner;
+
 //Test project for git
 
 public class GitTest {
 	public static void main(String[] args) {
-		String msg = "Test";
-		System.out.println(msg);
+		System.out.println("1...\n");
+		System.out.println("2...");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String scan = "";
+		
+		do {
+			scan = "";
+			scan = sc.nextLine();
+			
+			if (scan.equals("1")) {
+				System.out.println("Hello world");
+			}
+
+			if (scan.equals("2")) {
+				System.out.println("Hello world2");
+			}
+			
+		} while (!scan.equals("0"));
+				
 	}
 }
