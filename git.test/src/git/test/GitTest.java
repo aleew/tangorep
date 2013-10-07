7272c6a6cb86f8ce24d3ef7c04a6dff1a255b0ac
Samu Lehtonen <samu.lehtonen@hotmail.com>
2013-10-07 17:01:18 +0300

    Muutoksia testimielessä

--- git.test/src/git/test/GitTest.java
+++ git.test/src/git/test/GitTest.java
@@ -1,7 +1,10 @@
 package git.test;
 
+//Test project for git
+
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		String msg = "Test";
+		System.out.println(msg);
 	}
 }
