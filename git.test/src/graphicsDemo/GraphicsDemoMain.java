6d15ada7bbcc59db033872a89b4cae8f51bfdad2
Samu Lehtonen <samu.lehtonen@hotmail.com>
2013-10-09 15:31:09 +0300

    Reworked the initial structure of the application. Application now has different packages for different windows. Other windows are now called through main window which now doesn't contain any code related to other windows and their operations. Old packages should be removed from the project.

---
+++ git.test/src/graphicsDemo/GraphicsDemoMain.java
@@ -0,0 +1,104 @@
+package graphicsDemo;
+
+import interfaces.Index;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class GraphicsDemoMain extends JFrame implements ActionListener{
+
+	// Initialize panel
+	private GraphicsDemoDrawPanel graphicsDemoDrawPanel = new GraphicsDemoDrawPanel();
+
+	// Timer
+	Timer timer = new Timer();
+
+	// Initialize buttons
+	private JButton coordinatesButton = new JButton("New coordinates");
+	private JButton colorButton = new JButton("New color");
+	private JButton goHamButton = new JButton("GO HAM!");
+	
+	public GraphicsDemoMain() {
+		super.setTitle("Graphics demo");
+		
+		// JPanel for buttons
+		JPanel buttonBar = new JPanel();
+
+		// Add buttons and listeners for them
+		buttonBar.add(coordinatesButton);
+		coordinatesButton.addActionListener(this);
+		buttonBar.add(colorButton);
+		colorButton.addActionListener(this);
+		buttonBar.add(goHamButton);
+		goHamButton.addActionListener(this);
+
+		// Add JPanels to JFrame
+		add(buttonBar, BorderLayout.NORTH);
+		add(graphicsDemoDrawPanel, BorderLayout.CENTER);
+		
+		this.setSize(400, 200);
+		
+		this.setResizable(false);
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+String command = evt.getActionCommand();
+		
+		//Handling the pressing of buttons
+		if(command.equals("New coordinates")) {
+			
+			//Timer is cancelled and purged when new button os pressed
+			timer.cancel();
+			timer.purge();
+			
+			graphicsDemoDrawPanel.setNewCoordinates();
+			repaint();
+		}
+		
+		else if(command.equals("New color")) {
+			
+			//Timer is cancelled and purged when new button os pressed
+			timer.cancel();
+			timer.purge();
+			graphicsDemoDrawPanel.setDrawingColor();
+			repaint();
+		}
+		
+		else if(command.equals("GO HAM!")) {
+			
+			//Timer is cancelled and purged when new button os pressed
+			timer.cancel();
+			timer.purge();
+			
+			//Reinitialize timer
+			timer = new Timer();
+			
+			//Timer runs in 200ms intervals and generates
+			//new coordinates and color for the drawing
+			timer.scheduleAtFixedRate(new TimerTask(){
+
+				@Override
+				public void run() {
+
+					graphicsDemoDrawPanel.setDrawingColor();
+					graphicsDemoDrawPanel.setNewCoordinates();
+					repaint();
+					
+				}
+				
+			},200, 200);
+		}
+		
+	}
+
+}
---
+++ git.test/src/mainWindow/Index.java
@@ -0,0 +1,69 @@
+package mainWindow;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import graphicsDemo.GraphicsDemoMain;
+
+public class Index extends JFrame implements ActionListener{
+
+	public Index() {
+		super.setTitle("Main window");
+		
+		//Panels for the window
+		JPanel welcomeText = new JPanel();
+		JPanel buttonBar = new JPanel();
+		
+		//Header for the window
+		JLabel welcomeLabel = new JLabel("Live or Die...Make Your Choice");
+		welcomeText.add(welcomeLabel);
+		
+		//Buttons
+		JButton graphicsDemoButton = new JButton("Graphics demo");
+		graphicsDemoButton.addActionListener(this);
+		buttonBar.add(graphicsDemoButton);
+		
+		add(welcomeText, BorderLayout.NORTH);
+		add(buttonBar, BorderLayout.CENTER);
+			
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		String command = evt.getActionCommand();
+		
+		//Code for the button presses
+		if(command.equals("Graphics demo")) {
+			GraphicsDemoMain graphicsDemo = new GraphicsDemoMain();
+			graphicsDemo.setVisible(true);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		//Create the window
+		Index mainWindow = new Index();
+		
+		//Window size
+		mainWindow.setSize(400, 200);
+		
+		//Don't allow resizing
+		mainWindow.setResizable(false);
+		
+		//Close application if this window is closed
+		mainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		//Set window visible
+		mainWindow.setVisible(true);
+		
+		
+	}
+
+	
+}
